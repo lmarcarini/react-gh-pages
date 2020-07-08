35d2c2e39257d839f8578f559edeadb6cdd4c51a
lmarcarini <leonardo.marcarini@yahoo.com.br>
2020-07-08 20:53:50 -0300

    Updates

---
+++ precache-manifest.de59426d118605325c728598a92987d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8006cea802d20411b1ee017b6800c500",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "92a0be6a8ca3a8af39f5",
+    "url": "/react-gh-pages/static/css/main.ededc79a.chunk.css"
+  },
+  {
+    "revision": "91e8c4c6646b1cc97f9b",
+    "url": "/react-gh-pages/static/js/2.606fd9f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.606fd9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a0be6a8ca3a8af39f5",
+    "url": "/react-gh-pages/static/js/main.d68668b9.chunk.js"
+  },
+  {
+    "revision": "57e1e3363fc6cce8badd",
+    "url": "/react-gh-pages/static/js/runtime-main.1fe65d36.js"
+  },
+  {
+    "revision": "179ee91f9eb6ba43e328687f15fb1c24",
+    "url": "/react-gh-pages/static/media/FilterIcon.179ee91f.png"
+  },
+  {
+    "revision": "38961a0363e05d8954193d2744c9c660",
+    "url": "/react-gh-pages/static/media/addIcon.38961a03.svg"
+  },
+  {
+    "revision": "4085798acdaa10abe7a1631e0c62b18d",
+    "url": "/react-gh-pages/static/media/banner.4085798a.jpg"
+  },
+  {
+    "revision": "9f3b504d50ac7ff90f8d19c880bf5964",
+    "url": "/react-gh-pages/static/media/heartempty.9f3b504d.png"
+  },
+  {
+    "revision": "8dacde495bc6a11fe5b5b8a42a8389fe",
+    "url": "/react-gh-pages/static/media/heartfull.8dacde49.png"
+  },
+  {
+    "revision": "fd07ed86f0025215972f209d515559b0",
+    "url": "/react-gh-pages/static/media/logo.fd07ed86.png"
+  }
+]);
